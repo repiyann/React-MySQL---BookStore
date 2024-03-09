3baa2689aa09b3d6528fc44e30d151664bc6bad6
repiyann <muhrepiyan26@gmail.com>
2024-03-09 21:15:31 +0700

    feat(modal): add bookModal in booksCard

--- frontend/src/components/home/booksCard.jsx
+++ frontend/src/components/home/booksCard.jsx
@@ -1,12 +1,16 @@
+import { useState } from 'react'
 import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
 import { PiBookOpenTextLight } from 'react-icons/pi'
-import { BiUserCircle } from 'react-icons/bi'
+import { BiUserCircle, BiShow } from 'react-icons/bi'
 import { AiOutlineEdit } from 'react-icons/ai'
 import { BsInfoCircle } from 'react-icons/bs'
 import { MdOutlineDelete } from 'react-icons/md'
+import BookModal from './bookModal'
 
 const BooksCard = ({ books }) => {
+	const [showModal, setShowModal] = useState(false)
+
 	return (
 		<div className="grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
 			{books.map((book) => (
@@ -25,6 +29,10 @@ const BooksCard = ({ books }) => {
 						<h2 className="my-1">{book.author}</h2>
 					</div>
 					<div className="flex justify-between items-center gap-x-2 mt-4 p-4">
+						<BiShow
+							className="text-3xl text-blue-800 hover:text-black cursor-pointer"
+							onClick={() => setShowModal(true)}
+						/>
 						<Link to={`/books/detail/${book.id}`}>
 							<BsInfoCircle className="text-2xl text-green-800 hover:text-black" />
 						</Link>
@@ -35,6 +43,12 @@ const BooksCard = ({ books }) => {
 							<MdOutlineDelete className="text-2xl text-red-600 hover:text-black" />
 						</Link>
 					</div>
+					{showModal && (
+						<BookModal
+							book={book}
+							onClose={() => setShowModal(false)}
+						/>
+					)}
 				</div>
 			))}
 		</div>
