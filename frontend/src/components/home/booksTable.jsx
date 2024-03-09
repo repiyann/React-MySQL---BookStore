5ab4ddfb31d161bdf42df09036ae8cb13aa683a6
repiyann <muhrepiyan26@gmail.com>
2024-03-09 20:22:38 +0700

    feat(table&card): add table and card for home

--- frontend/src/components/home/booksTable.jsx
+++ frontend/src/components/home/booksTable.jsx
@@ -1,5 +1,5 @@
-import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
 import { AiOutlineEdit } from 'react-icons/ai'
 import { BsInfoCircle } from 'react-icons/bs'
 import { MdOutlineDelete } from 'react-icons/md'
